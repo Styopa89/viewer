3a1d277de04990dd6d5758c73a166a29f4f59794
styopa89 <styopa.v@gmail.com>
2017-04-14 17:04:03 +0300

    SHAP-SHOT

--- src/main/java/com/vstepura/web/controller/HelloController.java
+++ src/main/java/com/vstepura/web/controller/HelloController.java
@@ -1,10 +1,11 @@
 package com.vstepura.web.controller;
 
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
-@HelloController
 
 public class HelloController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
